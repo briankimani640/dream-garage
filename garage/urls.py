d0c6b4a641abf600d7f6e0a4264a2f84ffb87632
Brian Kimani <briankimani640@gmail.com>
2026-03-19 16:33:07 +0300

    feat: Add vehicle list view, URL routing, and Bootstrap template

---
+++ garage/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # This makes the vehicle list show up right on the homepage
+    path('', views.vehicle_list, name='vehicle_list'), 
+]
--- garage/views.py
+++ garage/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Vehicle
 
-# Create your views here.
+def vehicle_list(request):
+    # To fetch all vehicles from the database, ordered by the newest added first
+    vehicles = Vehicle.objects.all().order_by('-added_on')
+    
+    # Pass the data to the template using a context dictionary
+    context = {
+        'vehicles': vehicles
+    }
+    return render(request, 'garage/vehicle_list.html', context)
